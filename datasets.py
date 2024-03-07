d9183178c6b01ea07265b3f4c3b4b7734a5f74ca
Latortuga96 <fteste96@protonmail.com>
2024-03-07 15:18:32 +0100

    remove unnecessary import in datasets.py

--- datasets.py
+++ datasets.py
@@ -10,22 +10,15 @@ import argparse
 
 import rasterio
 import glob
-#from torch.masked import masked_tensor, as_masked_tensor
 import warnings
 
 # Disable prototype warnings and such
 warnings.filterwarnings(action='ignore', category=UserWarning)
 
-import os
-import rasterio
-import numpy as np
-import torch
-
           
 args = parse_args()
 
 DEFAULT_DIR = "Dataset_nomask/" +  args.month
-Yield_path = "~/Documents/PhD/Donnees/dataframe/Corn_belt/GPP/GPPmt/yield"
 
 class GLASS_TrainDataset(Dataset):
     def __init__(self,img_size):
@@ -47,10 +40,7 @@ class GLASS_TrainDataset(Dataset):
             min_pixel_values = np.nanmin(image, axis=(1, 2))
             max_pixel_values = np.nanmax(image, axis=(1, 2))
             image_normalized = (image.astype(np.float32) - min_pixel_values[:, np.newaxis, np.newaxis]) / (max_pixel_values[:, np.newaxis, np.newaxis] - min_pixel_values[:, np.newaxis, np.newaxis])
-            # Standardize each band separately
-        # image_tensor = torch.from_numpy(image_normalized)
-        # mask = torch.isnan(image_tensor)
-        # image_masked_tensor = masked_tensor(image_tensor, ~mask) 
+ 
         
         return image_normalized  
       
@@ -77,10 +67,8 @@ class GLASS_TestDataset(Dataset):
             min_pixel_values = np.nanmin(image, axis=(1, 2))
             max_pixel_values = np.nanmax(image, axis=(1, 2))
             image_normalized = (image.astype(np.float32) - min_pixel_values[:, np.newaxis, np.newaxis]) / (max_pixel_values[:, np.newaxis, np.newaxis] - min_pixel_values[:, np.newaxis, np.newaxis])
-            # Standardize each band separately
-        # image_tensor = torch.from_numpy(image_normalized)
-        # mask = torch.isnan(image_tensor)
-        # image_masked_tensor = masked_tensor(image_tensor, ~mask)  
+     
+        
 
         return image_normalized  
     
@@ -115,42 +103,3 @@ def get_test_dataloader(args):
         )
 
     return test_dataloader, test_dataset
-
-# class Yield_Dataset(Dataset):
-#     def __init__(self):
-#         # Replace 'Yield_path' with the actual path to the directory containing the CSV file
-#         self.data_dir = "Yield_path"
-#         csv_path = os.path.join(self.data_dir, "yieldb_July_yearly_variation.csv")
-#         self.data_frame = pd.read_csv(csv_path)
-
-#     def __len__(self):
-#         return len(self.data_frame)
-
-#     def __getitem__(self, index):
-#         # Retrieve the first column value (assuming column name is 'first_column_name')
-#         Yield = torch.tensor(self.data_frame.iloc[index, 0], dtype=torch.float32)
-
-        
-#         return Yield
-    
-
-# # # Step 2: Create a custom data loader for LOOCV
-# # class LeaveOneOutLoader:
-# #     def __init__(self, dataset, batch_size=1):
-# #         self.dataset = dataset
-# #         self.batch_size = batch_size
-
-# #     def __iter__(self):
-# #         for i in range(len(self.dataset)):
-# #             training_set = self.dataset[:i] + self.dataset[i + 1:]
-# #             test_sample = self.dataset[i]
-
-# #             training_loader = Yield_Dataset(training_set, batch_size=self.batch_size, shuffle=True)
-# #             test_loader = Yield_Dataset([test_sample], batch_size=1)
-
-# #             yield training_loader, test_loader
-
-# #     def __len__(self):
-# #         return len(self.dataset)
-         
-
